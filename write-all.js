fe83d260447c7bb126ba86082183a04161b4d24f
Brian Grinstead <briangrinstead@gmail.com>
2022-12-13 13:37:32 -0800

    disable due to schema change upstream

--- write-all.js
+++ write-all.js
@@ -2,5 +2,5 @@ import "./write-comments.js";
 import "./write-issues.js";
 import "./write-caniuse.js";
 import "./write-mdn.js";
-import "./write-chromestatus.js";
+// import "./write-chromestatus.js";
 import "./write-mozilla-positions.js";
