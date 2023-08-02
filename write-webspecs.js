9683b880a9dfea7ede78a1a3224a99e1e04f6c51
Brian Grinstead <briangrinstead@gmail.com>
2023-08-01 21:20:34 -0700

    more webspecs

--- write-webspecs.js
+++ write-webspecs.js
@@ -1,11 +1,70 @@
 import specs from "https://raw.githubusercontent.com/w3c/browser-specs/main/index.json" assert { type: "json" };
 
-let orgs = new Set(specs.map(s => s.organization));
-let orgsMap = new Map([...orgs].map(o => [o, specs.filter(s => s.organization === o).length]));
+// Schema at https://github.com/w3c/browser-specs
+// See also https://www.specref.org/
+
+let orgs_map = new Map(
+  [...new Set(specs.map((s) => s.organization))].map((o) => [
+    o,
+    specs.filter((s) => s.organization === o).length,
+  ])
+);
+let standing_map = new Map(
+  [...new Set(specs.map((s) => s.standing))].map((o) => [
+    o,
+    specs.filter((s) => s.standing === o).length,
+  ])
+);
+let series_map = new Map(
+  [...new Set(specs.map((s) => s.seriesComposition))].map((o) => [
+    o,
+    specs.filter((s) => s.seriesComposition === o).length,
+  ])
+);
+let repo_url_map = new Map(
+  [
+    ...new Set(
+      specs
+        .filter((s) => s.nightly.repository && s.nightly.sourcePath)
+        .map((s) => new URL(s.nightly.repository + s.nightly.sourcePath).host)
+    ),
+  ].map((o) => [
+    o,
+    specs.filter(
+      (s) =>
+        s.nightly.repository &&
+        s.nightly.sourcePath &&
+        new URL(s.nightly.repository).host === o
+    ).length,
+  ])
+);
+let groups_map = new Map(
+  [...new Set([...specs.map((s) => s.groups.map((g) => g.name))])].map((o) => [
+    o,
+    specs.filter((s) => s.groups.find((g) => g.name == o)).length,
+  ])
+);
 
 let output = {
-  specs_per_org: Object.fromEntries(orgsMap),
-}
+  specs_per_org: Object.fromEntries(orgs_map),
+  specs_per_standing: Object.fromEntries(standing_map),
+  specs_per_series: Object.fromEntries(series_map),
+  specs_per_group: Object.fromEntries(groups_map),
+  specs_per_repo_host: Object.fromEntries(repo_url_map),
+};
+
+console.log(
+  specs.length,
+  specs.filter((spec) => spec.nightly.repository).length
+);
+console.log(specs.length, specs.filter((spec) => spec.nightly.url).length);
+console.log(specs.length, specs.filter((spec) => spec.nightly.filename).length);
+console.log(
+  specs.length,
+  specs.filter((spec) => spec.nightly.sourcePath).length
+);
+
+console.log(specs.filter((spec) => !spec.nightly.repository));
 Deno.writeTextFileSync(
   "./output/webspecs_summary.json",
   JSON.stringify(output, null, 2)
